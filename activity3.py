2e5862c9816365578be6980cba52a706e3ae691e
Bharath007-hub <kidsidbad@gmail.com>
2025-04-16 12:21:49 +0530

    hi

---
+++ activity3.py
@@ -0,0 +1,9 @@
+import datetime
+
+current_time = datetime.datetime.now()
+
+print("time now at greenwith meridian is : ", end = "")
+print(current_time)
+
+import calendar
+print("\n", calendar.calendar(2025))
